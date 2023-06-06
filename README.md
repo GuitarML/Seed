cf31df8192b3c250273645a8d3e449676428fa32
Keith <smartguitarml@gmail.com>
2023-06-06 06:36:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# NeuralSeed
+
+Neural Seed uses neural networks to emulate amps/pedals on the Daisy Seed hardware by [Electro-Smith](https://www.electro-smith.com/), and the Terrarium
+pedal by [PedalPCB](https://www.pedalpcb.com/product/pcb351/). Models are trained on audio recordings from your amp or pedal, and can be uploaded to the Daisy Seed
+as firmware. NeuralSeed includes several built in amp/pedal models, as well as controls for input/output level, wet/dry mix, and up to 3 parameterized knobs
+for the selected neural model. Effective for Amps/PreAmps (direct out, no cab), Distortion/Overdrive/Boost pedals (non-time based, no Reverb/Delay/Flange/Phaser).
+
+Download the neuralseed.bin for Daisy Seed from the [Releases](https://github.com/GuitarML/NeuralSeed/releases) page. See release notes for a list of built in amp/pedal models.
+
+Start training models for NeuralSeed the easy way using the [Colab Script](https://colab.research.google.com/github/GuitarML/Automated-GuitarAmpModelling/blob/ns-capture/ProteusCapture.ipynb)!
+
+![app](https://github.com/GuitarML/NeuralSeed/blob/main/neuralseed.jpg)
+
+The Daisy Seed is a powerful ARM Cortex-M7 powered board intended for audio effects. The Terrarium pcb provides input and output buffers, as well as connections
+to the Daisy Seed for up to 6 potentiometer knobs, 4 switches, in/out jacks, and 9v power supply. NeuralSeed is the software developed by GuitarML to run on this hardware. 
+Total material cost was less than $100 to build a fully capable digital guitar multi-effect.
+
+For information on compiling NeuralSeed in the Daisy Ecosystem see the [Daisy Wiki](https://github.com/electro-smith/DaisyWiki/wiki).
+
+## Technical Info
+In comparison to other GuitarML plugins, Neural Seed is very minimal, running only a GRU size 10 (by comparison
+[Proteus](https://github.com/GuitarML/Proteus) uses LSTM size 40). Using this size model, may not be able to accurately capture certain devices, especially
+high gain amps, but should work decently well for distortion/overdrive pedals and low gain amps (direct out, not
+from a microphone). This is due to limited processing power on the M7 microcontroller. The current code processes on
+float32 audio data, so this could be optimized for the M7 chip using quantized int16 data. The Daisy Seed
+supports the addition of a display so this could be a future modification, as well as an encoder knob to switch models.
+
+The compiled binary and model data fits into Flash Memory, which is limited to 128KB. The [RTNeural](https://github.com/jatinchowdhury18/RTNeural)
+engine is used for fast inferencing of the neural models with a very tiny footprint.  It is possible to add more models utilizing other data storage 
+areas on the Daisy Seed.
+
+Utilizing only the 128KB Flash memory, NeuralSeed can hold around 15 different models. The initial release includes 11 built in models,
+and there is room to add more, however the lack of a display means it's up to you to keep track of what model you're on!
+
+## Getting started
+Build the daisy libraries with:
+```
+make -C DaisySP
+make -C libDaisy
+```
+
+Then flash your terrarium with the following commands (or use the [Electrosmith Web Programmer](https://electro-smith.github.io/Programmer/))
+```
+cd your_pedal
+# using USB (after entering bootloader mode)
+make program-dfu
+# using JTAG/SWD adaptor (like STLink)
+make program
+```
+
+# Control
+
+| Control | Description | Comment |
+| --- | --- | --- |
+| Ctrl 1 | Input Level | Adjusts the input level to the neural net model (0 to 3x volume) |
+| Ctrl 2 | Mix | Set the dry/wet amount (full left for dry, full right for only neural net output |
+| Ctrl 3 | Volume | Sets the output volume of the pedal |
+| Ctrl 4 | Neural Param 1 | Adjusts the first model parameter, such as gain/tone (only applies to 1-conditioned models) |
+| Ctrl 5 | Neural Param 2 | Adjusts the second model parameter, such as gain/tone (only applies to 2-conditioned models)  |
+| Ctrl 6 | Neural Param 3 | Adjusts the third model parameter, such as gain/tone (only applies to 3-conditioned models) |
+| SW 1 - 4 | 4Band EQ Boost | Bass, Low Mid, High Mid, Treble Boost (Band Pass) |
+| FS 1 | Bypass/Active | Bypass / effect engaged |
+| FS 2 | Cycle Neural Model | Loads the next available Neural Model, starts at beginning after the last in the list (i.e. next pedal/amp) |
+| LED 1 | Bypass/Active Indicator |Illuminated when effect is set to Active |
+| LED 2 | Brightness based on selected model parameterization | Off = Snapshot, Dim = 1 Knob, Brighter = 2 Knobs, Brightest = 3 Knobs |
+| Audio In 1 | Audio input | Mono only for Terrarium |
+| Audio Out 1 | Mix Out | Mono only for Terrarium |
+
+## Training your own Models (Amp/Pedal Capture)
+
+You can train models for Neural Seed using the GuitarML fork (ns-capture branch) of [Automated-GuitarAmpModelling](https://github.com/GuitarML/Automated-GuitarAmpModelling/tree/ns-capture) code.
+Run "scripts/convert_json_to_c_header.py" on the resulting JSON model to generate a .h file, which you can copy and paste the weights into the "all_model_data.h" header before
+compiling "neuralseed.bin". Only GRU size 10 models (snapshot, 1-param) or GRU size 8 (2-param, 3-param) models are currently compatible. Train models using 48kHz to match Daisy Seed processing 
+(note that this is different from most other GuitarML plugins). A 48kHz input wav file based on the Proteus input wav is available [here](https://github.com/GuitarML/Automated-GuitarAmpModelling/blob/ns-capture/Data/Proteus_Capture_48k.wav).
+
+Keep in mind that capture with such small models is hit or miss, and may take some massaging to get a proper capture, or may not be captured at all. Loss values over 0.08 probably won't sound good,
+or may contain harmonic noise. Ensure to start with volume low when testing a new model on the pedal.
+
+## Adding your model to NeuralSeed
+To add your models to a custom build of Neural Seed, run the converter script on your json model. <br>
+```python convert_json_to_c_header_gru.py <model_file.json>```<br>
+Then copy and paste the information from the generated c-header into the ```all_model_data.h``` file 
+before compiling. Ensure to modify the "Model" weights to a unique identifier, and add to the initialization list a the top and the "model_collection" vector at the bottom.
+
+Email either the .json or .h model file to smartguitarml@gmail.com and it may be included in the official repo.
---
+++ src/neuralseed.cpp
@@ -0,0 +1,419 @@
+#include "daisy_petal.h"
+#include "daisysp.h"
+#include "terrarium.h"
+#include <RTNeural/RTNeural.h>
+
+// Model Weights (edit this file to add model weights trained with Colab script)
+#include "all_model_data_gru9_4count.h"
+
+using namespace daisy;
+using namespace daisysp;
+using namespace terrarium;  // This is important for mapping the correct controls to the Daisy Seed on Terrarium PCB
+
+// Declare a local daisy_petal for hardware access
+DaisyPetal hw;
+Parameter Gain, Level, Mix, reverbTime, delayTime_tremFreq, delayFdbk_tremDepth;
+bool            bypass;
+int             modelInSize;
+unsigned int    modelIndex;
+bool            pswitches[4];
+int             switches[4];
+float           nnLevelAdjust;
+
+bool            effects_only_mode;
+bool            switch1_hold;
+
+Led led1, led2;
+
+float     mix_amp;
+float     mix_effects;
+
+// Looper Parameters
+Looper looper;
+#define MAX_SIZE (48000 * 60 * 4) // 4 minutes of floats at 48 khz
+float DSY_SDRAM_BSS buf[MAX_SIZE];
+Oscillator led_osc; // For pulsing the led when recording
+//int pressCount;
+
+
+// Tremolo
+Tremolo tremolo;
+int waveform;
+float pTremDepth, pTremFreq;  // Previous values, for detecting changes from knob
+
+// Reverb
+ReverbSc  verb;
+float pReverbTime;          // Previous values, for detecting changes from knob
+
+
+// Delay
+#define MAX_DELAY static_cast<size_t>(48000 * 1.f) // 1 second max delay
+DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS delayLine;
+
+
+struct delay
+{
+    DelayLine<float, MAX_DELAY> *del;
+    float                        currentDelay;
+    float                        delayTarget;
+    float                        feedback;
+    float                        active = false;
+    
+    float Process(float in)
+    {
+        //set delay times
+        fonepole(currentDelay, delayTarget, .0002f);
+        del->SetDelay(currentDelay);
+
+        float read = del->Read();
+        if (active) {
+            del->Write((feedback * read) + in);
+        } else {
+            del->Write((feedback * read)); // if not active, don't write any new sound to buffer
+        }
+
+        return read;
+    }
+};
+
+delay delay1;
+
+
+// These are each of the Neural Model options.
+// GRU (Gated Recurrent Unit) networks with input sizes ranging 1 - 4.
+// Currently only using snapshot models, they tend to sound better and 
+//   we can use input level as gain.
+
+RTNeural::ModelT<float, 1, 1,
+    RTNeural::GRULayerT<float, 1, 9>,
+    RTNeural::DenseT<float, 9, 1>> model;
+
+// Notes: With default settings, GRU 10 is max size currently able to run on Daisy Seed
+//        - Parameterized 1-knob GRU 10 is max, GRU 8 with effects is max
+//        - Parameterized 2-knob/3-knob at GRU 8 is max
+//        - With multi effect (reverb, etc.) added GRU 9 is recommended to allow for processing space
+//        - These models should be trained using 48kHz audio data, since Daisy uses 48kHz by default.
+//             Models trained with other samplerates, or running Daisy at a different samplerate will sound off.
+
+// Loads a new model based on the first two switch positions, total of 4 possible combinations
+// Models 0 -> 3 
+//    (0=off, 1=on) 11=0, 01=1,  10=2,  00=3
+void changeModel()
+{
+    unsigned int modelIndex_temp = 0;
+
+    if (pswitches[0] == true && pswitches[1] == true ) {
+        modelIndex_temp = 0;
+        nnLevelAdjust = 1.0;
+    } else if (pswitches[0] == false && pswitches[1] == true) {
+        modelIndex_temp = 1;
+        nnLevelAdjust = 1.0;
+    } else if (pswitches[0] == true && pswitches[1] == false) {
+        modelIndex_temp = 2;
+        nnLevelAdjust = 1.0;
+    } else if (pswitches[0] == false && pswitches[1] == false) {
+        modelIndex_temp = 3;
+        nnLevelAdjust = 0.6;
+    }
+    if ( modelIndex_temp > (model_collection.size() - 1) ) {  // If model is not available, don't change model
+        return;
+    } else {
+        modelIndex = modelIndex_temp;
+
+        auto& gru = (model).template get<0>();
+        auto& dense = (model).template get<1>();
+        modelInSize = 1;
+        gru.setWVals(model_collection[modelIndex].rec_weight_ih_l0);
+        gru.setUVals(model_collection[modelIndex].rec_weight_hh_l0);
+        gru.setBVals(model_collection[modelIndex].rec_bias);
+        dense.setWeights(model_collection[modelIndex].lin_weight);
+        dense.setBias(model_collection[modelIndex].lin_bias.data());
+        model.reset();
+
+    }
+}
+
+
+void UpdateButtons()
+{
+
+    // (De-)Activate bypass and toggle LED when left footswitch is let go, or enable/disable amp if held for greater than 1 second //
+    // Can only disable/enable amp when not in bypass mode
+    if(hw.switches[Terrarium::FOOTSWITCH_1].FallingEdge())
+    {
+        if (switch1_hold) {
+            switch1_hold = false;
+        } else {
+            bypass = !bypass;
+            led1.Set(bypass ? 0.0f : 1.0f);
+        }
+    }
+
+    if(hw.switches[Terrarium::FOOTSWITCH_1].TimeHeldMs() >= 1000 && !bypass && !switch1_hold) { 
+        effects_only_mode = !effects_only_mode;
+        switch1_hold = true;  // This is to make sure effects_only_mode is only toggled once
+        if (!effects_only_mode) {
+            led1.Set(bypass ? 0.0f : 1.0f);
+        }
+    }
+
+
+
+    //switch2 pressed
+    if(hw.switches[Terrarium::FOOTSWITCH_2].RisingEdge())
+    {
+        looper.TrigRecord();
+        led2.Set(looper.Recording() ? 0.0f : 1.0f); // Turn on LED when loop is playing but not recording
+        if (!pswitches[3]) {
+            looper.SetReverse(true);
+        }
+    }
+
+    // If switch2 is held, clear the looper and turn off LED      TODO: Make LED blink twice when cleared
+    if(hw.switches[Terrarium::FOOTSWITCH_2].TimeHeldMs() >= 1000)
+    {
+        looper.Clear();
+        led2.Set(looper.Recording() ? 1.0f : 0.0f); 
+    }
+}
+
+void UpdateSwitches()
+{
+    // Select appropriate model based on 2 switch positions ////
+    bool model_changed = false;
+    for(int i=0; i<2; i++)
+        if (hw.switches[switches[i]].Pressed() != pswitches[i]) {
+            pswitches[i] = hw.switches[switches[i]].Pressed();
+            model_changed = true;
+        }
+    if (model_changed) {    
+        changeModel();
+    }
+
+    // Select appropriate effects mode for lower 3 knobs based on 3rd switch (either Reverb/Tremolo, or 3BandEQ) ////
+    if (hw.switches[switches[2]].Pressed() != pswitches[2]) {
+            pswitches[2] = hw.switches[switches[2]].Pressed();
+    }
+
+    // Reverse the loop when 4th switch is engaged
+    if (hw.switches[switches[3]].Pressed() != pswitches[3]) {
+        pswitches[3] = hw.switches[switches[3]].Pressed();
+        looper.SetReverse(!pswitches[3]);
+    }
+}
+
+// This runs at a fixed rate, to prepare audio samples
+static void AudioCallback(AudioHandle::InputBuffer  in,
+                          AudioHandle::OutputBuffer out,
+                          size_t                    size)
+{
+    //hw.ProcessAllControls();
+    hw.ProcessAnalogControls();
+    hw.ProcessDigitalControls();
+
+    UpdateButtons();
+
+    if (looper.Recording()) {
+        led2.Set(led_osc.Process()*0.5 + 0.5);  // Pulse the LED when recording
+    } 
+   
+    if (effects_only_mode && !bypass) {
+        led1.Set(led_osc.Process()*0.5 + 0.5);  // Pulse the LED when in effects only mode and not bypassed (opposite pulse of looper led)
+        //led1.Set(1.0 - led_osc.Process() * 0.5 + 0.5);
+    }
+
+    UpdateSwitches();
+
+    led1.Update();
+    led2.Update();
+
+    float input_arr[2] = { 0.0, 0.0 };
+    //float input_arr[1] = { 0.0 };    // Neural Net Input
+    float sendl, sendr, wetl, wetr;  // Reverb Inputs/Outputs
+    float delay_out;
+
+    // Get current knob parameters ////////////////////
+    float in_level = Gain.Process();
+    float out_level = Level.Process(); 
+    float mix_effects = Mix.Process();
+
+    float vReverbTime = reverbTime.Process();
+    float vdelayTime_tremFreq = delayTime_tremFreq.Process();
+    float vdelayFdbk_tremDepth = delayFdbk_tremDepth.Process();
+
+    // Check for changes in effects knobs /////////
+
+    // REVERB //
+    if ((pReverbTime != vReverbTime))
+    {
+        if (vReverbTime < 0.02) { // if knob < 2%, set reverb to 0
+            verb.SetFeedback(0.0);
+        } else {
+            verb.SetFeedback(vReverbTime * 0.5 + 0.5); // Reverb time range 0.5 to 1.0
+        }
+        pReverbTime = vReverbTime;
+    }
+
+    // DELAY //
+    if (pswitches[2] == true) {
+
+        if (vdelayTime_tremFreq < 0.02) {   // if knob < 2%, set delay to inactive
+            delay1.active = false;
+        } else {
+            delay1.active = true;
+        }
+        delay1.delayTarget = 2400 + vdelayTime_tremFreq * 45600 ; // in samples 50ms to 1 second  // Note: changing delay time with heavy reverb creates a cool modulation effect
+        delay1.feedback = vdelayFdbk_tremDepth;
+    
+    // TREMOLO //
+    } else {
+
+        if (pTremFreq != vdelayTime_tremFreq)
+        {
+            tremolo.SetFreq(vdelayTime_tremFreq * 12.0 + 0.5); // change range from 0-1 to 0.5 - 12.5 Hz
+            pTremFreq = vdelayTime_tremFreq;
+        }
+
+        if (pTremDepth != vdelayFdbk_tremDepth)
+        {
+            tremolo.SetDepth(vdelayFdbk_tremDepth);
+            pTremDepth = vdelayFdbk_tremDepth;
+        }
+    }
+
+    // Loop through the current audio block and process all effects ////////
+    // Order of effects is:
+    //           Gain -> Neural Model -> Delay ->
+    //           Reverb -> Effects Mixer -> Tremolo -> Looper 
+    //
+
+    for(size_t i = 0; i < size; i++)
+    {
+
+        // Process your signal here
+        if(bypass)
+        {
+            /// Process Looper // Able to use looper when in bypass mode // 
+            float loop_out;
+            loop_out = looper.Process(in[0][i]);
+            out[0][i] = in[0][i] + loop_out;
+        }
+        else
+        {   
+            float ampOut;
+            input_arr[0] = in[0][i] * in_level;
+
+            // Process Neural Net Model //
+            if (effects_only_mode) {
+                ampOut = input_arr[0] * 1.5; // apply gain level to dry signal and slight boost to match NN model levels
+            } else {
+               
+                ampOut = model.forward (input_arr) + input_arr[0];   // Run Model and add Skip Connection; CHANGE FROM v0.1, was calculating skip wrong, should have also added input gain, fixed here
+                ampOut *= nnLevelAdjust; // Manually adjust model volume for quiet or loud models TODO: move to Model parameter
+            }
+            
+            // Process Delay
+            delay_out = delay1.Process(ampOut);  
+
+            // Process Reverb //
+            float final_effects = 0.0;
+            sendl = (ampOut + delay_out) * 0.5;  // adding in pre-effects signal to go to reverb, volume reduction on effects input for better balance
+            sendr = sendl;
+            verb.Process(sendl, sendr, &wetl, &wetr);
+
+            final_effects = wetl + delay_out;
+
+            // Process Wet/Dry Effects Mix //
+            float final_effects_mix;
+            final_effects_mix = ampOut * (1.0- mix_effects) + final_effects * mix_effects; // Mix amp out with delay/reverb
+
+            final_effects_mix = tremolo.Process(final_effects_mix);   // Process Tremolo on total output (not controlled by effects mix, depth control acts as effect level)
+            final_effects_mix *= out_level;                           // Set output level
+
+            /// Process Looper //
+            float loop_out;
+            loop_out = looper.Process(final_effects_mix);
+            out[0][i] = loop_out + final_effects_mix;
+        }
+    }
+}
+
+
+int main(void)
+{
+    float samplerate;
+
+    hw.Init();
+    samplerate = hw.AudioSampleRate();
+
+    tremolo.Init(samplerate);
+    verb.Init(samplerate);
+
+    setupWeights();
+    //hw.SetAudioBlockSize(4);  // TODO try lower block sizes for better latency
+    hw.SetAudioBlockSize(32);
+
+    looper.Init(buf, MAX_SIZE);
+    looper.SetMode(Looper::Mode::NORMAL);
+    led_osc.Init(samplerate);
+    led_osc.SetFreq(12);  // Is this right? led updated every buffer, so 1000 times per second instead of 48k as expected, so setting to 48hz would show up as 1Hz?
+    led_osc.SetWaveform(1);
+
+    //pressCount = 0;
+    switch1_hold = false;
+
+    pswitches[0]= true;
+    pswitches[1]= true;
+    pswitches[2]= true;
+    pswitches[3]= true;
+
+    switches[0]= Terrarium::SWITCH_1;
+    switches[1]= Terrarium::SWITCH_2;
+    switches[2]= Terrarium::SWITCH_3;
+    switches[3]= Terrarium::SWITCH_4;
+
+    Gain.Init(hw.knob[Terrarium::KNOB_1], 0.0f, 2.0f, Parameter::LINEAR);
+    Mix.Init(hw.knob[Terrarium::KNOB_2], 0.0f, 1.0f, Parameter::LINEAR);
+    Level.Init(hw.knob[Terrarium::KNOB_3], 0.0f, 1.0f, Parameter::LINEAR); 
+    reverbTime.Init(hw.knob[Terrarium::KNOB_4], 0.0f, 1.0f, Parameter::LINEAR);
+    delayTime_tremFreq.Init(hw.knob[Terrarium::KNOB_5], 0.0f, 1.0f, Parameter::LINEAR);
+    delayFdbk_tremDepth.Init(hw.knob[Terrarium::KNOB_6], 0.0f, 1.0f, Parameter::LINEAR); 
+
+    // Initialize the correct model
+    modelIndex = 0;
+    changeModel();
+    nnLevelAdjust = 1.0;
+
+    // Initialize & set params for mixers 
+    mix_effects = 0.5;
+    effects_only_mode = false;
+
+    tremolo.SetFreq(3.0);
+    tremolo.SetDepth(0.0); 
+    tremolo.SetWaveform(0);   // WAVE_SIN = 0, WAVE_TRI = 1, WAVE_SAW = 2, WAVE_RAMP = 3, WAVE_SQUARE = 4
+
+    verb.SetFeedback(0.0);
+    verb.SetLpFreq(9000.0);
+
+    delayLine.Init();
+    delay1.del = &delayLine;
+    delay1.delayTarget = 2400; // in samples
+    delay1.feedback = 0.0;
+    delay1.active = false;     // Default to no delay
+
+    // Init the LEDs and set activate bypass
+    led1.Init(hw.seed.GetPin(Terrarium::LED_1),false);
+    led1.Update();
+    bypass = true;
+
+    led2.Init(hw.seed.GetPin(Terrarium::LED_2),false);
+    led2.Update();
+
+    hw.StartAdc();
+    hw.StartAudio(AudioCallback);
+    while(1)
+    {
+        // Do Stuff Infinitely Here
+        System::Delay(10);
+    }
+}
